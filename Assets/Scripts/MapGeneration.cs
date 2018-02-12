8159c85ed208bde3a1b0c14f3bfc2c321379b1a2
drunkbearzz <mlelek94@gmail.com>
2018-02-12 22:31:42 +0100

    Initial release-Basic traveling to position created

---
+++ Assets/Scripts/MapGeneration.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGeneration : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+        Instantiate(ResourceManager.GetPlanetObject(), new Vector2(4,4), Quaternion.identity, GameObject.Find("TravelScreen").transform);
+        RadialPlanetSpawn(200, 5, PlanetTypes.e_type.ICE);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    void RadialPlanetSpawn(float maxRadiusSpawn, float density, PlanetTypes.e_type planetType)
+    {
+        float layerDistance = 0.1f;
+        for(float distanceFromCenter = 1f; distanceFromCenter < maxRadiusSpawn; distanceFromCenter += layerDistance)
+        {
+            float distancePercentFactor = 1 - distanceFromCenter / maxRadiusSpawn;
+            if(distancePercentFactor > Random.Range(0f, 1f))
+                Instantiate(ResourceManager.GetPlanetObject(), new Vector2(distanceFromCenter, 0), Quaternion.identity, GameObject.Find("TravelScreen").transform);
+        }
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/SpaceTravelerBehaviour.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceTravelerBehaviour : MonoBehaviour {
+
+    private enum e_travelState { TRAVELING, NOT_TRAVELING }
+    e_travelState trevelState;
+
+    Vector2 pointToTravelTo;
+    float maxTravelSpeed;
+    float timeOfMovementStart;
+    float timeToReachMaxSpeed;
+    float stopMovementTargetRadius;
+    float slownDownMovementRadius;
+
+    // Use this for initialization 
+    void Start () {
+        trevelState = e_travelState.NOT_TRAVELING;        
+        maxTravelSpeed = 5f;
+        timeOfMovementStart = Time.time;
+        timeToReachMaxSpeed = 1.3f;
+        stopMovementTargetRadius = 0.25f;
+        slownDownMovementRadius = 2.2f;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (trevelState == e_travelState.TRAVELING)
+        {
+            if (IsInTravelTargetRadius())
+                trevelState = e_travelState.NOT_TRAVELING;
+            else if (IsInSlowDownRadius())
+                transform.Translate(Vector2.up * GetCurrentTravelSpeed() * Time.deltaTime * GetSLownDownFactor());
+            else
+                transform.Translate(Vector2.up * GetCurrentTravelSpeed() * Time.deltaTime);
+
+        }
+        else
+        {
+
+        }
+
+        if (Input.GetMouseButtonDown(0) && trevelState==e_travelState.NOT_TRAVELING)
+        {
+            pointToTravelTo = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RotateTowardsMouse();
+            ResetMovementStartTime();
+            trevelState = e_travelState.TRAVELING;            
+        }
+		
+	}
+
+
+
+    float GetCurrentTravelSpeed()
+    {
+        return (Time.time - timeOfMovementStart > timeToReachMaxSpeed) ? maxTravelSpeed : (Time.time - timeOfMovementStart) / timeToReachMaxSpeed * maxTravelSpeed;
+    }
+
+    bool IsInTravelTargetRadius()
+    {
+        return (Vector2.Distance(transform.position, pointToTravelTo) < stopMovementTargetRadius) ? true : false;
+    }
+    
+    bool IsInSlowDownRadius()
+    {
+        return (Vector2.Distance(transform.position, pointToTravelTo) < slownDownMovementRadius) ? true : false;
+    }
+    void RotateTowardsMouse()
+    {
+        Vector2 unitVecToDestination = (pointToTravelTo - (Vector2)transform.position).normalized;
+        float rotZ = Mathf.Atan2(unitVecToDestination.y, unitVecToDestination.x) * Mathf.Rad2Deg - 90;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ);
+    }
+    float GetSLownDownFactor()
+    {
+        return Vector2.Distance(transform.position, pointToTravelTo) / slownDownMovementRadius;
+    }
+    void ResetMovementStartTime()
+    {
+        timeOfMovementStart = Time.time;
+    }
+
+
+
+
+
+}
+
+
+
+
+
