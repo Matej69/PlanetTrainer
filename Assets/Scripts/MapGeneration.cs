873b97ee17d6a0e91ecea226ebe9ee90f014a022
drunkbearzz <mlelek94@gmail.com>
2018-02-13 00:02:45 +0100

    Circural planet generation done

--- Assets/Scripts/MapGeneration.cs
+++ Assets/Scripts/MapGeneration.cs
@@ -6,9 +6,8 @@ public class MapGeneration : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        
-        Instantiate(ResourceManager.GetPlanetObject(), new Vector2(4,4), Quaternion.identity, GameObject.Find("TravelScreen").transform);
-        RadialPlanetSpawn(200, 5, PlanetTypes.e_type.ICE);
+
+        RadialPlanetSpawn(new Vector2(0,0), 40, 5, PlanetTypes.e_type.ICE, "IcePlanetGroup");
     }
 	
 	// Update is called once per frame
@@ -17,14 +16,26 @@ public class MapGeneration : MonoBehaviour {
 	}
 
 
-    void RadialPlanetSpawn(float maxRadiusSpawn, float density, PlanetTypes.e_type planetType)
+    void RadialPlanetSpawn(Vector2 centerPos, float maxRadiusSpawn, float density, PlanetTypes.e_type planetType, string planetGroupName)
     {
-        float layerDistance = 0.1f;
-        for(float distanceFromCenter = 1f; distanceFromCenter < maxRadiusSpawn; distanceFromCenter += layerDistance)
+        const float minDistanceFromCenter = 4f; //used since density close to center is huge(planets would be to close to each other)
+        float layerDistance = 1f; //distance between 2 neighbourhood layers on which planets might be spawn
+        GameObject planets = new GameObject(planetGroupName);   //Parent GameObject of all instantiated objects
+
+        for(float distanceFromCenter = minDistanceFromCenter; distanceFromCenter < maxRadiusSpawn; distanceFromCenter += layerDistance)    //loop through every layer
         {
-            float distancePercentFactor = 1 - distanceFromCenter / maxRadiusSpawn;
-            if(distancePercentFactor > Random.Range(0f, 1f))
-                Instantiate(ResourceManager.GetPlanetObject(), new Vector2(distanceFromCenter, 0), Quaternion.identity, GameObject.Find("TravelScreen").transform);
+            for(int i = 0; i < density; ++i)    //Try to spawn this many planets on current layer
+            {
+                float distancePercentFactor = 1 - distanceFromCenter / maxRadiusSpawn;  //it gets lower, the chance that planet will be spawn are also lower
+                bool shouldPlanetBeSpawn = (distancePercentFactor > Random.Range(0f, 1f)) ? true : false;
+                if (shouldPlanetBeSpawn)
+                {
+                    float randomAngle = Random.Range(0, 360) * Mathf.Deg2Rad;   //sin and cos function work with radians
+                    Vector2 randomPosOnLayer = new Vector2(distanceFromCenter * Mathf.Cos(randomAngle) + centerPos.x, distanceFromCenter * Mathf.Sin(randomAngle) + centerPos.y);   //calculate position of coordinate with given angle
+                    Instantiate(ResourceManager.GetPlanetObject(), randomPosOnLayer, Quaternion.identity, planets.transform);
+                }
+            }
+            
         }
     }
 
